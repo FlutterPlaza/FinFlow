a1b6c7407f47d6c90b66c895aaf919378e4e9b80
TikuGaelle <brendagaelle6@gmail.com>
2022-12-21 20:17:04 +0100

    feat: home page design without scan icon

--- lib/homePage/HomePage.dart
+++ lib/homePage/HomePage.dart
@@ -20,14 +20,38 @@ class _MyHomePageState extends State<MyHomePage> {
       padding: const EdgeInsets.all(10.0),
       child: Column(
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(
+   radius: 30.0,
+   backgroundImage:NetworkImage('https://picsum.photos/200/300'),   
+     backgroundColor: Colors.transparent,), 
+              Row(
+              //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(FpbIcons.eye_open, 
+                  size: 20,
+                  color: AppColors.secondaryColorW,),
+                  SizedBox(
+                    width: 10,
+                  ),
+                   Icon(FpbIcons.face_id, 
+                  size: 20,
+                  color: AppColors.secondaryColorW,),
+                   SizedBox(
+                    width: 10,
+                  ),
+                   Icon(FpbIcons.notification, 
+                  size: 20,
+                  color: AppColors.secondaryColorW,),
+                ],
+              )
+            ],
+          ),
           SizedBox(
-            height: 50,
-          ),
-          Row(),
-          const SizedBox(
-            height: 20,
-          ),
-          //
+            height: 10,
+          ), 
           Card(
             elevation: 1,
             shape: RoundedRectangleBorder(
@@ -53,14 +77,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
+                    children:   [
                       Text(
                         '\$ 1.280.45',
-                        //  style: Theme.of(context).textTheme.displayMedium?.copyWith(
-                        //   color: AppColors.secondaryColorW,
-                        //           fontWeight: FontWeight.bold,
-                        //           fontSize: size.width * 0.043,
-                        //  ),
+                         style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                          color: AppColors.secondaryColorW,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: size.width * 0.023,
+                         ),
                       ),
                       Icon(
                         FpbIcons.eye_open,
@@ -76,25 +100,27 @@ class _MyHomePageState extends State<MyHomePage> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       ElevatedButton(
-                          // style: ButtonStyle(
-                          //   backgroundColor: MaterialStateProperty<Color>(Colors.grey)
-                          // ),
+                          //git style: Theme.elevatedbuttontheme
                           onPressed: () {},
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
+                            children:   [
                               Icon(
                                 Icons.add,
                                 size: 5,
                                 color: Colors.blue,
                               ),
-                              const Text(
+                                Text(
                                 "Add money",
-                                style: TextStyle(
-                                  fontSize: 7,
-                                ),
+                                 style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                          color: AppColors.primaryColorW,
+                                  //fontWeight: FontWeight.bold,
+                                  fontSize: size.width * 0.013, 
+                                // TextStyle(
+                                //   fontSize: 7,
+                                // ),
                               ),
-                            ],
+                           ), ],
                           )),
                       const SizedBox(
                         width: 30,
@@ -105,13 +131,13 @@ class _MyHomePageState extends State<MyHomePage> {
                           // ),
                           onPressed: () {},
                           child: Row(
-                            children: [
+                            children: const [
                               Icon(
                                 Icons.add,
                                 size: 5,
                                 color: Colors.blue,
                               ),
-                              const Text(
+                                Text(
                                 "Withdraw Funds",
                                 style: TextStyle(fontSize: 7),
                               ),
@@ -129,17 +155,6 @@ class _MyHomePageState extends State<MyHomePage> {
           Stack(
             children: [
               Align(
-                alignment: Alignment(1.0, .3),
-                child: Container(
-                  height: size.height * 0.3,
-                  width: size.width * 0.5,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              Align(
                 alignment: Alignment(.2, .2),
                 child: Container(
                   height: size.height * 0.3,
@@ -147,9 +162,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     color: AppColors.accentColorW,
+                    boxShadow: [
+      BoxShadow(
+        color: AppColors.accentColorW.withOpacity(0.6),
+        spreadRadius: 5,
+        blurRadius: 7,
+        offset: Offset(5, -5), // changes position of shadow
+      ),
+    ],
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.all(20),
+                    padding: const EdgeInsets.all(10),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -176,57 +199,49 @@ class _MyHomePageState extends State<MyHomePage> {
                           ],
                         ),
                         const SizedBox(
-                          height: 20,
+                          height: 10,
                         ),
                         const Text("Card Number"),
                         const SizedBox(
-                          height: 10,
+                          height: 8,
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        Column(
                           children: [
-                            Column(
-                              children: [
-                                fourDots(),
-                                const SizedBox(height: 20),
-                                const Text('Empty'),
-                                const SizedBox(
-                                  height: 3,
-                                ),
-                                const Text('04/2025')
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                fourDots(),
-                                const SizedBox(height: 20),
-                                const Text('ccv'),
-                                const SizedBox(
-                                  height: 3,
-                                ),
-                                const Text('123')
-                              ],
-                            ),
-                            Column(children: [
-                              fourDots(),
-                              const SizedBox(
-                                height: 60,
-                              )
-                            ]),
-                            Column(
+                            Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                const Text("1234"),
-                                const SizedBox(
-                                  height: 35,
-                                ),
-                                Icon(
-                                  FpbIcons.eye_open,
-                                  color: AppColors.primaryColorW,
-                                  size: 20,
-                                )
+                                fourDots(),
+                                fourDots(),
+                                fourDots(),
+                                Text("1234"), 
                               ],
-                            )
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Row(
+                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [ 
+                                    Text("Empty"),
+                                    Text("CCV"),
+                                    Text("          "),
+                                    Text(" "),
+                                ]),
+
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children:  [
+                                      Text("04/2025"),
+                                      Text("123"),
+                                      Text("             "),
+                                    Icon(
+                                      FpbIcons.eye_open,
+                                      color: AppColors.primaryColorW,
+                                      size: 20,
+                                    )
+                                  ],
+                                )
                           ],
                         )
                       ],
@@ -235,13 +250,112 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ],
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: AppColors.cardColorW,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20) ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Latest Activity"),
+                        Container(
+                          height: 20,
+                          width: 20,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.grey,
+                          ),
+                          child: Center(child: Text("2")),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: size.height * 0.35,
+                      child: ListView.builder(
+                        itemCount: 2,
+                        itemBuilder: (BuildContext context, int index){
+                          return activityCard();
+                        }
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+         // activityCard(),
         ],
       ),
     );
   }
 }
-
+Widget activityCard (){
+  return Container(
+    height: 55,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      //color: AppColors.cardColorW,
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+           // crossAxisAlignment: CrossAxisAlignment.start,
+           // mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              CircleAvatar(
+   radius: 25.0,
+   backgroundImage: NetworkImage('https://picsum.photos/200/300'),   
+       backgroundColor: Colors.transparent,),
+           SizedBox(
+            width: 25,
+           ),
+       Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("@john_merry",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          )),
+          Text("August 3, 14:05",
+          style: TextStyle(
+            fontSize: 13
+          ))
+        ],
+       ),
+        ],
+          ), 
+       Column(
+        children: [
+          Text("\$ 14.6"),
+          Icon(Icons.favorite_border_sharp,
+          color: AppColors.secondaryColorW,
+          size: 12,)
+        ],
+       )
+  
+          // SvgPicture.asset(
+          //   SvgNames.authBackground )
+        ],
+      ),
+    ),
+  );
+}
 Widget fourDots() {
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
