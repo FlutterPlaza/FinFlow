15336fb4470ff9d0c7e0605d6b2f5c4fccae857b
Amanda  Shafack <shafack.likhene@gmail.com>
2023-02-09 22:18:25 +0100

    refactor: rename classes to reflect the KISS principle

--- lib/onboarding/view/splash_screen.dart
+++ lib/onboarding/view/splash_screen.dart
@@ -3,10 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fpb/authentication_with_firebase/application/bloc/auth_bloc.dart';
 import 'package:fpb/core/settings/cached.dart';
-import 'package:fpb/home_screen/view/home_screen.dart';
 import 'package:fpb/injection.dart';
 import 'package:fpb/onboarding/view/onboarding_screens.dart';
-import 'package:fpb/sign_in/view/sign_in_page.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
--- widgetbook/home_page/home_page.dart
+++ widgetbook/home_page/home_page.dart
@@ -4,6 +4,6 @@ import 'package:widgetbook/widgetbook.dart';
 WidgetbookUseCase get homePageUsecase => WidgetbookUseCase(
       name: 'home-page_usecase',
       builder: (context) {
-        return const MyHomePage();
+        return const Home();
       },
     );
