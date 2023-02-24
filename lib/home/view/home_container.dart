961337a079025e5f25aecb9e686d0533f30046a4
DesmondTambe <desking2000@gmail.com>
2023-02-24 20:50:10 +0100

    feat: create user_signup screen

--- lib/home/view/home_container.dart
+++ lib/home/view/home_container.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fpb/home/application/home_view_bloc/home_view_bloc.dart';
 import 'package:fpb/home/view/dashboard.dart';
+import 'package:fpb/home/view/user_seacrh.dart';
 import 'package:fpb/savings/view/savings_page.dart';
 
 class HomeContainer extends StatelessWidget {
@@ -16,7 +17,7 @@ class HomeContainer extends StatelessWidget {
           savings: (_) => SavingsPage(),
           quickCash: (_) => Container(child: Center(child: Text('budget'))),
           budget: (_) => Container(child: Center(child: Text('budget'))),
-          search: (_) => Container(child: Center(child: Text('search'))),
+          search: (_) => UserSearch(),
         );
       },
     );
---
+++ lib/home/view/user_seacrh.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserSearch extends StatelessWidget {
+  const UserSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
