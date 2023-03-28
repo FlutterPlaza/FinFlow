d8687e981f9a29a74ce526b87226ca639fa832c4
Amanda  Shafack <shafack.likhene@gmail.com>
2023-03-28 14:52:48 +0100

    chore: fix access to applocalization
    - Enable access to an instance of AppLocalization from the test file
    - Rename the onboarding screen integration test file

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,7 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'bottom_nav_bar_test.dart';
-import 'onboarding/view/onboarding_screens.dart';
+import 'onboarding/view/onboarding_flow_test.dart';
+
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
