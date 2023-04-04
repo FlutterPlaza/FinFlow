bb686d4eaeb2c8942c9e299b1e0d2584599eb0dc
Amanda  Shafack <shafack.likhene@gmail.com>
2023-04-04 13:09:33 +0100

    chore: redirect the login button to the home screen without auth
    The current implemented authentication features do not work in
    production.
    
    Link the login button to the home screen without valid authentication
    checks to validate the UIs within the app.

--- lib/sign_in/view/widgets/login_button.dart
+++ lib/sign_in/view/widgets/login_button.dart
@@ -1,9 +1,12 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
 import 'package:fpb/core/application/email_password_bloc/email_password_bloc.dart';
+import 'package:fpb/core/domain/user.dart';
 import 'package:fpb/core/presentation/widget/fpb_button.dart';
 import 'package:fpb/l10n/l10n.dart';
+import 'package:fpb/router/app_route.gr.dart';
 
 class LoginButton extends StatelessWidget {
   const LoginButton({
@@ -13,17 +16,24 @@ class LoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
+
+    /// TODO: Remove after general app UI validation
+    final User user = User(id: 'BZHyLm22EUai8ISKY43RBjMyjPB3', isNewUser: false, photo: 'https://lh3.googleusercontent.com/a/AGNmyxZqAyvq47qEAmNuIlrG3kc3pAVurxcFBZ3gAdTIEf4=s96-c', providerId: '', name: 'Amanda Shafack,', email: 'shafack.likhene@gmail.com', phoneNumber: '');
+
     return BlocBuilder<EmailPasswordBloc, EmailPasswordState>(
       buildWhen: (previous, current) => previous.status != current.status,
       builder: (context, state) {
-        return FormzStatus.submissionInProgress == state.status
-            ? const FpbButton(label: 'Log in', onTap: null)
-            : FpbButton(
-                label: l10n.signInLogInButtonLabel,
-                onTap: () => context
-                    .read<EmailPasswordBloc>()
-                    .add(const EmailPasswordEvent.submitted()),
-              );
+        // return FormzStatus.submissionInProgress == state.status
+        //     ?  FpbButton(label: 'Log in', onTap: null)
+        //     : FpbButton(
+        //         label: l10n.signInLogInButtonLabel,
+        //         onTap: () => context
+        //             .read<EmailPasswordBloc>()
+        //             .add(const EmailPasswordEvent.submitted()),
+        //       );
+
+        /// TODO Short circuit Login button, remove after general app UI validation
+        return FpbButton(label: 'Log in', onTap: (){context.router.push(HomeRouter(user: user ));});
       },
     );
   }
